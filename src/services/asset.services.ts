071f2996eb3cfd1e7c7eddaa88be37119aeaf6e8
mendingNgodeng <helmitiger7@gmail.com>
2026-03-05 20:16:50 +0700

    add enable register config

--- src/services/asset.services.ts
+++ src/services/asset.services.ts
@@ -5,7 +5,7 @@ export class assetService {
 
   static async getAll() {
     return prisma.asset.findMany({
-    orderBy:{created_at:'asc'},
+    orderBy:{created_at:'desc'},
       include:{
         type:{
           select:{name:true}
