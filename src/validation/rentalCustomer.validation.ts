68a745ab041996c8a2a3662edbaca636702128b0
mendingNgodeng <helmitiger7@gmail.com>
2026-03-06 12:24:45 +0700

    small fixes on rental customer validation message

--- src/validation/rentalCustomer.validation.ts
+++ src/validation/rentalCustomer.validation.ts
@@ -2,8 +2,8 @@ import { z } from 'zod';
 export const Schema = z.object({
   name: z
     .string()
-    .min(6, 'tidak boleh kosong')
-    .max(40, 'name maksimal 40 karakter'),
+    .min(6, 'Minimal 6 Karakter')
+    .max(50, 'Maksimal maksimal 50 karakter'),
   phone: z.string().min(1,"Nomor HP tidak boleh kosong"),
   pictureKtp: z.string().optional()
               
@@ -12,7 +12,7 @@ export const Schema = z.object({
 export const UpdateSchema = z.object({
   name: z
     .string()
-    .min(6, 'tidak boleh kosong')
-    .max(40, 'name maksimal 40 karakter'),
+    .min(6, 'Minimal 6 Karakter')
+    .max(50, 'Maksimal maksimal 50 karakter'),
   phone: z.string().min(1,"Nomor HP tidak boleh kosong"),
 });
